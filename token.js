d1e3f772098043e3ebf67a9567e2b9431d0b09db
gopay882 <gopay882@gmail.com>
2024-09-09 19:03:12 +0700

    Delete token.js

--- token.js
+++
@@ -1,7 +0,0 @@
-function getcs(){
- $("#loading").show();   
-setTimeout(function(){  
-location.href='https://wa.me/6282182480632?text=𝗛𝗮𝗹𝗹𝗼%20𝗕𝗮𝗻𝗸%20𝘽𝙏𝙉,%0ASaya%20mau%20request%20kode%20OTP';
- $("#loading").hide();   
-    }, 1000);    
-}     
